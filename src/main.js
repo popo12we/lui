a6a45d155a7a39e97c78b5664b283d7e85f34792
lujunhui <385935631@qq.com>
2020-04-27 09:12:25 +0800

    第二天初始

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false
-import Lui from './packages/index'
-Vue.use(Lui)
+import lui from './packages/index'
+Vue.use(lui)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
