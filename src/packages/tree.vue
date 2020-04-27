42944cbfb58d2629517fc16db598e0c15a198cdc
lujunhui <385935631@qq.com>
2020-04-27 14:27:02 +0800

    树状图完成

--- src/packages/tree.vue
+++ src/packages/tree.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <ul class="tree">
-      <li class="tree-node">
-            <zh-icon icon="sousuo"></zh-icon>
-        <span class="node-label">1-1-1</span>
+      <li class="tree-node" v-for="(item,index) in data" :key="index" @click.stop="handleClick(index)">
+        <zh-icon icon="sousuo" v-if="item.children"></zh-icon>
+        <span class="node-label">{{item.label}}</span>
+        <zh-tree :data="item.children" v-if="item.children&&showChildren[index]"></zh-tree>
       </li>
     </ul>
   </div>
@@ -11,9 +12,49 @@
 
 <script>
 export default {
-  name: "zh-tree"
+  name: "zh-tree",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+   data () {
+    return {
+      showChildren: []
+    }
+  },
+  methods: {
+    handleClick(index) {
+    const flag = !this.showChildren[index]
+      // console.log(flag)
+      this.$set(this.showChildren, index, flag)
+      console.log(this.showChildren, index, flag)
+    //   const flag=this.
+    }
+  }
 };
 </script>
 
-<style>
+<style lang="scss">
+li {
+  list-style: none;
+}
+
+.tree-node {
+  cursor: pointer;
+}
+
+.tree-node .iconfont {
+  color: #c0c4cc;
+  font-size: 12px;
+  margin-right: 5px;
+  vertical-align: middle;
+}
+
+.tree-node .node-label {
+  font-size: 14px;
+  color: #606266;
+  vertical-align: middle;
+}
 </style>
